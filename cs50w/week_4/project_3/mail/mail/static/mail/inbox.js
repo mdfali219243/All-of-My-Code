cd18edd801e70a219bf9f0776d29eca4e405a3ba
Md Foysal Ali <younous219243@gmail.com>
2025-07-19 22:20:41 -0500

    self web page

--- cs50w/week_4/project_3/mail/mail/static/mail/inbox.js
+++ cs50w/week_4/project_3/mail/mail/static/mail/inbox.js
@@ -28,6 +28,12 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  document.querySelector('#show_mail').innerHTML
+  fetch('/emails/indox')
 }
+
